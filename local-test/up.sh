405ce9fffede99f810933b906cde2cb4dd47eaf8
Christoph Schubert <christoph.schubert@tu-dortmund.de>
2022-09-10 07:17:55 +0200

    * Add health-check to Kafka Streams app * created up.sh and sample-data

---
+++ local-test/up.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+docker compose up -d
+
+echo "Waiting for broker to start up"
+sleep 20 # to prevent console producer to throw too many errors while Kafka is starting up
+
+kafka-console-producer --topic input --bootstrap-server localhost:9093 < sample-data
+
+docker compose restart word-count-app
+docker compose ps
+
+echo "Waiting for streams app to start"
+sleep 3
+
+echo "Kafka streams health: $(curl -s http://localhost:7171/health)"
+echo "Kafka streams topology:" && curl -s http://localhost:7171/topology
+echo "Kafka streams metrics (sample):" && curl -s http://localhost:7070/metrics | head -25
+
+echo "Kafka topics" && kafka-topics --list --bootstrap-server localhost:9093
--- src/main/java/net/christophschubert/kafka/streams/WordCountDemo.java
+++ src/main/java/net/christophschubert/kafka/streams/WordCountDemo.java
@@ -1,5 +1,7 @@
 package net.christophschubert.kafka.streams;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -10,12 +12,92 @@ import org.apache.kafka.streams.kstream.Produced;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Properties;
 
 public class WordCountDemo {
 
     private static final Logger log = LoggerFactory.getLogger(WordCountDemo.class);
+
+    public static final String HEALTH_CHECK_PORT_CONFIG = "health.check.port";
+    public static final String HEALTH_CHECK_PORT_DEFAULT = "7171";
+
+    private final HttpServer server;
+    private final KafkaStreams kafkaStreams;
+    private final Topology topology;
+
+    public WordCountDemo(Properties config) {
+        final var healthCheckPort = Integer.parseInt(config.getProperty(HEALTH_CHECK_PORT_CONFIG, HEALTH_CHECK_PORT_DEFAULT));
+        this.server = setupHealthChecks(healthCheckPort);
+        this.topology = buildTopology();
+        this.kafkaStreams = new KafkaStreams(this.topology, config);
+        kafkaStreams.start();
+    }
+
+    private void sendStringResponse(HttpExchange httpExchange, int resultCode, Object body) {
+        final var method = httpExchange.getRequestMethod();
+        final var uri = httpExchange.getRequestURI();
+        try {
+            final var response = Objects.toString(body).getBytes(StandardCharsets.UTF_8);
+
+            final var responseSize = response.length;
+            httpExchange.sendResponseHeaders(resultCode, responseSize);
+            try (OutputStream os = httpExchange.getResponseBody()) {
+                os.write(response);
+            }
+            log.info("Handled {} request for '{}' with status {}, send {} bytes.",
+                    method,
+                    uri,
+                    resultCode,
+                    responseSize
+            );
+        } catch (IOException e) {
+            log.error("Error handling {} request for '{}'", method, uri, e);
+        }
+    }
+
+    HttpServer setupHealthChecks(int healthCheckPort) {
+        try {
+            final var server = HttpServer.create(new InetSocketAddress(healthCheckPort), 0);
+
+            server.createContext("/topology", httpExchange -> {
+                if (topology == null) {
+                    sendStringResponse(httpExchange, 500, "Topology has not been created");
+                } else {
+                    sendStringResponse(httpExchange, 200, topology.describe());
+                }
+            });
+
+            server.createContext("/health", httpExchange -> {
+                if (kafkaStreams == null) {
+                    sendStringResponse(httpExchange, 500, "KafkaStreams instance has not been created");
+                } else {
+                    final var streamsState = kafkaStreams.state();
+                    final int returnCode = streamsState.isRunningOrRebalancing() ? 200 : 400;
+                    sendStringResponse(httpExchange, returnCode, streamsState);
+                }
+            });
+
+            new Thread(server::start).start();
+            log.info("Listening for health checks on port {}", healthCheckPort);
+            return server;
+        } catch (IOException e) {
+            throw new RuntimeException("Could not create HTTP server", e);
+        }
+    }
+
+    void shutDown() {
+        final var delayInSeconds = 5;//double-check: is this reasonable (for both resources)?
+        kafkaStreams.close(Duration.ofSeconds(delayInSeconds));
+        server.stop(delayInSeconds);
+    }
+
 
     Topology buildTopology() {
         final var builder = new StreamsBuilder();
@@ -36,11 +118,9 @@ public class WordCountDemo {
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
 
-        final var app = new WordCountDemo();
+        final var app = new WordCountDemo(config);
 
-        final var topology = app.buildTopology();
-        final var kafkaStreams = new KafkaStreams(topology, config);
-        kafkaStreams.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(app::shutDown));
         log.info("Kafka Streams app started");
     }
 }
